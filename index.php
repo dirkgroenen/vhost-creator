68789fd00fdb6d800e791ba1db2f6b9041eda9b1
Dirk Groenen <dirk@taketwo.nl>
2015-04-20 12:21:28 +0200

    fixed template and added php test

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <html>
-  <head>
-    <title></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  </head>
-  <body>
-    SITE created...
-  </body> </html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <b>HTML:</b> Site created...<br/>
+        <b>PHP:</b> <?php echo "Site created..."; ?>
+    </body>
 </html>
